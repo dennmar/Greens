67bcdef8a88087879274d1cf0ecd7760b529a834
dennmar <dennismariano3@gmail.com>
2020-03-21 16:18:16 -0700

    Add login button to lead to greens activity

--- app/src/main/java/com/example/dennis/greens/LoginActivity.java
+++ app/src/main/java/com/example/dennis/greens/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.dennis.greens;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +12,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_view);
+
+        Button loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent greensIntent = new Intent(LoginActivity.this,
+                        GreensActivity.class);
+
+                // prevent back button from going to login
+                greensIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                        Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(greensIntent);
+            }
+        });
     }
 
     @Override
