ee7e04c88b22a04818945c65c81cd726d3742f82
dennmar <dennismariano3@gmail.com>
2020-04-08 15:00:30 -0700

    Update user and expense routes for RESTful routing

--- api/server/routes/expense.py
+++ api/server/routes/expense.py
@@ -7,33 +7,27 @@ from ..models import expense
 
 bp = Blueprint('expense', __name__, url_prefix='/expense')
 
-@bp.route('/', methods=['GET'])
+@bp.route('/', methods=['GET', 'POST'])
 @login_required
 def expense_view():
-    """Find all expenses associated with the user.
+    """Find all the user's expenses or store a new expense for the user.
 
     Returns:
-        A flask.Response containing all the user's expenses.
+        A flask.Response containing all the user's expenses for a GET request
+        or containing the id of the created expense for a POST request.
     """
-    user_expenses = [exp.to_dict() for exp in current_user.expenses]
-    return flask.make_response({'expenses': user_expenses}, 200)
+    if request.method == 'GET':
+        user_expenses = [exp.to_dict() for exp in current_user.expenses]
+        return flask.make_response({'expenses': user_expenses}, 200)
+    elif request.method == 'POST':
+        request_json = request.get_json()
+        cost = request_json['cost']
+        description = request_json['description']
 
-@bp.route('/new', methods=['POST'])
-@login_required
-def new_expense_view():
-    """Store a new expense for the user.
+        # NOTE: need validation and error handling
+        new_expense = expense.Expense(user_id=current_user.id, cost=cost,
+            description=description)
+        db.session.add(new_expense)
+        db.session.commit()
 
-    Returns:
-        A flask.Response describing the result.
-    """
-    request_json = request.get_json()
-    cost = request_json['cost']
-    description = request_json['description']
-
-    # NOTE: need validation and error handling
-    new_expense = expense.Expense(user_id=current_user.id, cost=cost,
-        description=description)
-    db.session.add(new_expense)
-    db.session.commit()
-
-    return flask.make_response({'created_id': new_expense.id}, 200)
+        return flask.make_response({'created_id': new_expense.id}, 200)
--- api/server/routes/user.py
+++ api/server/routes/user.py
@@ -6,34 +6,29 @@ from ..models import user
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
-@bp.route('/', methods=['GET'])
+@bp.route('/', methods=['GET', 'POST'])
 def user_view():
-    """Return all users stored in database.
+    """Return all users or create a new user.
 
     Returns:
-        A flask.Response containing a list of all users.
+        A flask.Response containing a list of all users for a GET request or
+        containing the id of the created user for a POST request.
     """
-    # NOTE: should have pagination
-    users = [repr(u) for u in user.User.query.all()]
-    return flask.make_response({'users': users}, 200)
+    if request.method == 'GET':
+        # NOTE: should have pagination
+        users = [repr(u) for u in user.User.query.all()]
+        return flask.make_response({'users': users}, 200)
+    elif request.method == 'POST':
+        request_json = request.get_json()
+        username = request_json['username']
+        email = request_json['email']
+        password = request_json['password']
+        password_hash = user.User.genr_password_hash(password)
 
-@bp.route('/new', methods=['POST'])
-def user_new_view():
-    """Store a new user in the database.
+        # NOTE: need validation and error handling
+        new_user = user.User(username=username, email=email,
+            password=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
 
-    Returns:
-        A flask.Response containing the ID of the new user.
-    """
-    request_json = request.get_json()
-    username = request_json['username']
-    email = request_json['email']
-    password = request_json['password']
-    password_hash = user.User.genr_password_hash(password)
-    
-    # NOTE: need validation and error handling
-    new_user = user.User(username=username, email=email,
-        password=password_hash)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return flask.make_response({'created_id': new_user.id}, 200)
+        return flask.make_response({'created_id': new_user.id}, 200)
