0f215d1d839649991d1738c3f91afa643ce20f00
dennmar <dennismariano3@gmail.com>
2020-04-08 19:02:48 -0700

    Add show, edit, delete routes for a specific expense

--- api/server/models/user.py
+++ api/server/models/user.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    expenses = db.relationship('Expense', backref='user', lazy=True)
+    expenses = db.relationship('Expense', backref='user')
 
     @staticmethod
     def genr_password_hash(password):
--- api/server/routes/expense.py
+++ api/server/routes/expense.py
@@ -31,3 +31,43 @@ def expense_view():
         db.session.commit()
 
         return flask.make_response({'created_id': new_expense.id}, 200)
+
+@bp.route('/<int:expense_id>', methods=['GET', 'PUT', 'DELETE'])
+@login_required
+def specific_expense_view(expense_id):
+    """Return or update a specific expense (that is associated with the user).
+
+    Returns:
+        A flask.Response describing the result.
+    """
+    # NOTE: need error handling and session management
+    exp = expense.Expense.query.filter(expense.Expense.id == expense_id,
+       expense.Expense.user_id == current_user.id).first()
+
+    if request.method == 'GET':
+        if exp is not None:
+            return flask.make_response({'expense': exp.to_dict()}, 200)
+        else:
+            return flask.make_response({'expense': None}, 404)
+    elif request.method == 'PUT':
+        if exp is not None:
+            request_json = request.get_json()
+            cost = request_json['cost']
+            description = request_json['description']
+
+            exp.cost = cost
+            exp.description = description
+
+            db.session.merge(exp)
+            db.session.commit()
+
+            return flask.make_response({'updated_id': expense_id}, 200)
+        else:
+            return flask.make_response({'updated_id': None}, 404)
+    elif request.method == 'DELETE':
+        if exp is not None:
+            db.session.delete(exp)
+            db.session.commit()
+            return flask.make_response({'deleted_id': expense_id}, 200)
+        else:
+            return flask.make_response({'deleted_id': None}, 404)
