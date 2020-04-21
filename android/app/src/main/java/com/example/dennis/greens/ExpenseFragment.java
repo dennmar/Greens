ca9db44edd5ece99a80aef854a25488e1748fc80
dennmar <dennismariano3@gmail.com>
2020-04-21 08:57:18 -0700

    Add blank expense page with logout button

---
+++ android/app/src/main/java/com/example/dennis/greens/ExpenseFragment.java
@@ -0,0 +1,52 @@
+package com.example.dennis.greens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+public class ExpenseFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.expense_frag_view,
+                container, false);
+
+        Button logoutBtn = rootView.findViewById(R.id.expLogoutButton);
+
+        return rootView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void logout() {
+        Intent logoutIntent = new Intent(getActivity(), LoginActivity.class);
+
+        // prevent back button from going to login
+        logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(logoutIntent);
+    }
+}
+
